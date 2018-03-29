99ebeef7a46c5b4e1bfb289dfa20e2ddb53f51fc
luckyadam <weitaozsh@gmail.com>
2018-03-29 15:30:02 +0800

    fix: 样式单位规范

---
+++ alien.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projectName: 'alien-todo',
+  designWidth: 750,
+  weapp: {
+
+  },
+  h5: {
+
+  }
+}
---
+++ src/components/tab.js
@@ -0,0 +1,18 @@
+import Alien, { Component } from 'alienjs'
+
+class Tab extends Component {
+
+  componentDidMount () {
+
+  }
+
+  handlderChange () {
+
+  }
+
+  render () {
+    return null
+  }
+}
+
+export default Tab
