7be3b3025f2a191ed37d44525168bcd38aa1a5fb
luckyadam <weitaozsh@gmail.com>
2018-04-02 17:29:18 +0800

    feat: 同步更新配置

--- project.config.js
+++ project.config.js
@@ -1,10 +1,39 @@
 module.exports = {
   projectName: 'alien-todo',
   designWidth: 750,
+  plugins: {
+    babel: {
+      sourceMap: true,
+      presets: [
+        'env'
+      ],
+      plugins: [
+        'transform-class-properties',
+        'transform-decorators-legacy',
+        'transform-object-rest-spread'
+      ]
+    },
+  },
   weapp: {
 
   },
   h5: {
-
+    sourceRoot: 'src',
+    outputRoot: 'dist',
+    publicPath: '/',
+    // the directory contains images/fonts/media etc. files
+    staticDirectory: 'static',
+    // define global constants for application see https://webpack.js.org/plugins/define-plugin/
+    defineConstants: {
+    },
+    // support functions
+    module: {
+      postcss: {
+        // autoprefixer plugin config
+        autoprefixer: {
+          enable: true
+        }
+      }
+    }
   }
 }
