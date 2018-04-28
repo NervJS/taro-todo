35fa8fd6105d2a31cef61965a4753ec56710f7a1
peidan <m18814127307@163.com>
2018-04-28 14:08:56 +0800

    feat(image): add

--- src/pages/component/pages/image/image.js
+++ src/pages/component/pages/image/image.js
@@ -3,6 +3,7 @@ import { View, Text } from '@tarojs/components'
 import Head from '../../../../components/head/head'
 import Foot from '../../../../components/foot/foot'
 import './image.scss'
+import './nerv_logo.png'
 
 export default class PageView extends Component {
   config = {
@@ -26,7 +27,7 @@ export default class PageView extends Component {
               <Image
                 // mode='aspectfill'
                 style='width: 300px;height: 100px;background: #fff;'
-                src='https://camo.githubusercontent.com/3e1b76e514b895760055987f164ce6c95935a3aa/687474703a2f2f73746f726167652e333630627579696d672e636f6d2f6d74642f686f6d652f6c6f676f2d3278313531333833373932363730372e706e67'
+                src='nerv_logo.png'
               />
             </View>
           </View>
@@ -34,7 +35,6 @@ export default class PageView extends Component {
             <View className="page-section-title">
               <Text>Internet Image</Text>
             </View>
-            <View className="flex-wrp" style="flex-direction:column;">
               <View className="page-section-spacing">
                 <Image
                   // mode='aspectfill'
@@ -42,7 +42,6 @@ export default class PageView extends Component {
                   src='https://camo.githubusercontent.com/3e1b76e514b895760055987f164ce6c95935a3aa/687474703a2f2f73746f726167652e333630627579696d672e636f6d2f6d74642f686f6d652f6c6f676f2d3278313531333833373932363730372e706e67'
                 />
               </View>
-            </View>
           </View>
         </View>
         <Foot/>
