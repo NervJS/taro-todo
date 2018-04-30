db1d0b0a78e74d2616e70c8030ddddcf795d0c5e
peidan <m18814127307@163.com>
2018-04-30 18:18:32 +0800

    feat(video): video,audio

--- src/pages/component/pages/audio/audio.js
+++ src/pages/component/pages/audio/audio.js
@@ -16,29 +16,16 @@ export default class PageView extends Component {
         <Head title="audio" />
         <View className="page-body">
           <View className="page-section">
-            <View className="page-section-title">
-              <Text>flex-direction: row\n横向布局</Text>
-            </View>
             <View className="page-section-spacing">
-              {/*<Audio*/}
-                {/*src='http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46'*/}
-                {/*controls=true*/}
-                {/*autoplay=false*/}
-                {/*loop=false*/}
-                {/*muted=true*/}
-                {/*initialTime='30'*/}
-                {/*id='video'*/}
-              {/*/>*/}
-            </View>
-          </View>
-          <View className="page-section">
-            <View className="page-section-title">
-              <Text>flex-direction: column\n纵向布局</Text>
-            </View>
-            <View className="flex-wrp" style="flex-direction:column;">
-              <View className="flex-item flex-item-V demo-text-1" />
-              <View className="flex-item flex-item-V demo-text-2" />
-              <View className="flex-item flex-item-V demo-text-3" />
+              <Audio
+                src='http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46'
+                controls={true}
+                autoplay={false}
+                loop={false}
+                muted={true}
+                initialTime='30'
+                id='video'
+              />
             </View>
           </View>
         </View>
--- src/pages/component/pages/video/video.js
+++ src/pages/component/pages/video/video.js
@@ -3,42 +3,37 @@ import { View, Text } from '@tarojs/components'
 import Head from '../../../../components/head/head'
 import Foot from '../../../../components/foot/foot'
 import './video.scss'
+
 export default class PageView extends Component {
   config = {
     navigationBarTitleText: 'video'
   }
-  constructor() {
+
+  constructor () {
     super(...arguments)
   }
-  render() {
+
+  render () {
     return (
       <View className="container">
-        <Head title="video" />
+        <Head title="video"/>
         <View className="page-body">
           <View className="page-section">
-            <View className="page-section-title">
-              <Text>flex-direction: row\n横向布局</Text>
-            </View>
             <View className="page-section-spacing">
-              <View className="flex-wrp" style="flex-direction:row;">
-                <View className="flex-item demo-text-1" />
-                <View className="flex-item demo-text-2" />
-                <View className="flex-item demo-text-3" />
-              </View>
-            </View>
-          </View>
-          <View className="page-section">
-            <View className="page-section-title">
-              <Text>flex-direction: column\n纵向布局</Text>
-            </View>
-            <View className="flex-wrp" style="flex-direction:column;">
-              <View className="flex-item flex-item-V demo-text-1" />
-              <View className="flex-item flex-item-V demo-text-2" />
-              <View className="flex-item flex-item-V demo-text-3" />
+              <Video
+                src='http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+                controls={true}
+                autoplay={false}
+                poster='http://misc.aotu.io/booxood/mobile-video/cover_900x500.jpg'
+                initialTime='0'
+                id='video'
+                loop={false}
+                muted={false}
+              />
             </View>
           </View>
         </View>
-        <Foot />
+        <Foot/>
       </View>
     )
   }
