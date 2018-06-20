b68183aeb3e49136d05093db1aed2859347c2bd0
hejiasheng <szhejason@jd.com>
2018-06-20 15:11:01 +0800

    update progress

--- src/pages/component/pages/progress/progress.js
+++ src/pages/component/pages/progress/progress.js
@@ -1,36 +1,84 @@
 import './progress.scss'
 
 import { Component } from '@tarojs/taro'
-import { View, Progress } from '@tarojs/components'
+import { View, Progress, Button } from '@tarojs/components'
 
 import Header from '../../../../components/head/head'
 
 export default class PageView extends Component {
-  constructor () {
+  constructor() {
     super(...arguments)
+    this.timmer = null
   }
-  
-  render () {
+
+  state = {
+    progress: 0
+  }
+
+  handleStart = () => {
+    if (this.timmer) return
+    this.timmer = setInterval(() => {
+      this.setState({
+        process: ++this.state.progress
+      })
+    }, 500)
+  }
+
+  handleStop = () => {
+    if (this.timmer) {
+      clearImmediate(this.timmer)
+      this.timmer = null
+    }
+  }
+
+  handleReset = () => {
+    this.handleStop()
+    this.setState({
+      process: 0
+    })
+  }
+
+  render() {
+    const { progress } = this.state
     return (
       <View className='container'>
         <Header title='Progress'></Header>
         <View className='page-body'>
           <View className='page-section'>
-            <View className='page-section-spacing' style='margin-bottom:104px;'>
-              <Progress percent={20} showInfo strokeWidth={2} ></Progress>
+            <View className='progress-block'>
+              <View className='page-section-spacing'>
+                <Progress percent={20} showInfo strokeWidth={2} ></Progress>
+              </View>
             </View>
-            <View className='page-section-spacing'>
-              <Progress percent={40} strokeWidth={2} active ></Progress>
+            <View className='progress-block'>
+              <View className='page-section-spacing'>
+                <Progress percent={40} strokeWidth={2} active ></Progress>
+              </View>
+              <View className='page-section-spacing'>
+                <Progress percent={60} strokeWidth={2} active ></Progress>
+              </View>
+              <View className='page-section-spacing'>
+                <Progress
+                  percent={80}
+                  strokeWidth={2}
+                  active
+                  activeColor='blue' ></Progress>
+              </View>
             </View>
-            <View className='page-section-spacing'>
-              <Progress percent={60} strokeWidth={2} active ></Progress>
-            </View>
-            <View className='page-section-spacing'>
-              <Progress
-                percent={80}
-                strokeWidth={2}
-                active
-                activeColor='blue' />
+            <View>{progress}</View>
+            <View className='progress-block'>
+              <View className='page-section-spacing '>
+                <Progress
+                  percent={progress}
+                  strokeWidth={2}
+                  active
+                  activeColor='#3C7FE8' ></Progress>
+                <View>
+                  <Button onClick={this.handleStart}>加载</Button>
+                  <Button onClick={this.handleStop}>暂停</Button>
+                  <Button onClick={this.handleReset}>重置</Button>
+                </View>
+              </View>
             </View>
           </View>
         </View>
