41c41a2d702a050a20536d94eb56210b58be96bf
luckyadam <weitaozsh@gmail.com>
2018-06-22 16:18:14 +0800

    feat: 迁移出组件库 demo 成为一个纯粹的 todo

--- src/app.js
+++ src/app.js
@@ -1,37 +1,19 @@
+import '@tarojs/async-await'
 import Taro, { Component } from '@tarojs/taro'
+import { Provider } from '@tarojs/redux'
+
 import Index from './pages/index/index'
+import configStore from './store'
+
 import './app.scss'
+
+const store = configStore()
 
 class App extends Component {
   config = {
     pages: [
-      'pages/component/index',
-      'pages/component/pages/view/view',
-      'pages/component/pages/scroll-view/scroll-view',
-      'pages/component/pages/icon/icon',
-      'pages/component/pages/progress/progress',
-      'pages/component/pages/image/image',
-      'pages/component/pages/audio/audio',
-      'pages/component/pages/video/video',
-      'pages/component/pages/swiper/swiper',
-      'pages/component/pages/form/form',
-      'pages/component/pages/input/input',
-      'pages/component/pages/checkbox/checkbox',
-      'pages/component/pages/radio/radio',
-      'pages/component/pages/button/button',
-      'pages/component/pages/text/text',
-      'pages/component/pages/label/label',
-      'pages/component/pages/picker/picker',
-      'pages/component/pages/slider/slider',
-      'pages/component/pages/switch/switch',
-      'pages/component/pages/textarea/textarea',
-      'pages/component/pages/canvas/canvas',
-      'pages/component/pages/map/map',
-      'pages/component/pages/navigator/navigator',
-      'pages/component/pages/redirectPage/redirectPage',
-      'pages/component/pages/navigatePage/navigatePage',
-      // 'pages/index/index',
-      // 'pages/hello/hello',
+      'pages/index/index',
+      'pages/hello/hello',
       'pages/about/about'
     ],
     window: {
@@ -39,24 +21,6 @@ class App extends Component {
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'TODO List',
       navigationBarTextStyle: 'black'
-    },
-    tabBar: {
-      backgroundColor: '#fff',
-      selectedColor: '#dc0032',
-      list: [
-        {
-          pagePath: 'pages/component/index',
-          text: '组件',
-          iconPath: 'asset/component/view.png',
-          selectedIconPath: 'asset/component/view-red.png'
-        },
-        {
-          pagePath: 'pages/about/about',
-          text: '关于',
-          iconPath: 'asset/component/nav.png',
-          selectedIconPath: 'asset/component/nav-red.png'
-        }
-      ]
     }
   }
 
@@ -69,7 +33,11 @@ class App extends Component {
   componentCatchError () {}
 
   render () {
-    return <Index />
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
   }
 }
 
